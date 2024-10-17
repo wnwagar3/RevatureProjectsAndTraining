349db9cf2bb458efd44d0fc889b176a65d9bbdb3
Wagar <william.wagar@brightspeed.com>
2024-10-17 16:52:00 -0500

    Squashed 'wnwagar3-OuterJoin.git/' content from commit 048b1e9
    git-subtree-dir: wnwagar3-OuterJoin.git
    git-subtree-split: 048b1e9a7af9e7dd7f82964ed42087c1a61af136

---
+++ problem1.sql
@@ -0,0 +1,5 @@
+ SELECT * 
+ FROM class
+ LEFT OUTER JOIN student
+ ON class.class_title = student.class_title;
+ 
---
+++ problem2.sql
@@ -0,0 +1,6 @@
+SELECT * 
+FROM class
+RIGHT OUTER JOIN textbook
+ON class.class_title = textbook.class_title;
+
+
