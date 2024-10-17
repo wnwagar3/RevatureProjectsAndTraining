933406ed5233e9e53a73cd262e1f76d3bc1bbe9c
Wagar <william.wagar@brightspeed.com>
2024-10-17 16:41:43 -0500

    Squashed 'wnwagar3-InsertPosition.git/' content from commit 951134e
    git-subtree-dir: wnwagar3-InsertPosition.git
    git-subtree-split: 951134ebcdf16976d919fc4bc5936572bace4a1a

---
+++ src/main/java/InsertPosition.java
@@ -0,0 +1,36 @@
+
+
+public class InsertPosition {
+
+    /**
+     * Find the position that a number should be inserted into in a sorted array.
+     * For instance, in the array {2,4,6,8,10}, inserting 5 would belong at position 2, since the resulting array
+     * would be {2,4,5,6,8,10}, where the index of 5 is 2 (remember zero-indexing.)
+     * See the test cases for more examples.
+     *
+     * You may assume no duplicate values.
+     *
+     * @param nums an array of ints.
+     * @param target the number that would hypothetically be inserted.
+     * @return return the index that a number should be inserted into a sorted array.
+     */
+    public int searchInsert(int[] nums, int target) {
+        int arraySize = nums.length;
+        if (target < nums[0]) {
+            return 0;
+        }
+        for (int i = 0; i < nums.length -1;  i++) {
+            if (target == nums[i]) {
+                return i;
+            } else if (target > nums[i] && target < nums[i + 1]) {
+                return i + 1;
+            }
+        } 
+        if (target > nums[nums.length - 1]) {
+            return arraySize;
+        }
+        return -1;
+        }       
+}
+
+
