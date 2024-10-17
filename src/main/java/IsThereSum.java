af31cca4b337cff7a9c577517b83fb2b4d678014
Wagar <william.wagar@brightspeed.com>
2024-10-17 16:45:44 -0500

    Squashed 'wnwagar3-IsThereSum.git/' content from commit 37c431a
    git-subtree-dir: wnwagar3-IsThereSum.git
    git-subtree-split: 37c431a288829386555243718e3860383444ed1d

---
+++ src/main/java/IsThereSum.java
@@ -0,0 +1,22 @@
+
+public class IsThereSum {
+    /**
+     * Check if there exist two numbers in an array add up to some other number.
+     * You will likely need a for loop nested inside of another for loop for this problem - each for loop checking
+     * sums of two different indexes of arr.
+     *
+     * @param arr an array of ints.
+     * @param target a hypothetical sum of two numbers.
+     * @return true if two separate values in the array add up to a target, false otherwise.
+     */
+    public boolean check(int[] arr, int target){
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i+1; j < arr.length; j++) {
+                if(arr[i] + arr[j] == target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
