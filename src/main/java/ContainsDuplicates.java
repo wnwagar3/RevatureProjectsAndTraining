75ffa381fa042063b607c338db4852207811d5a3
Wagar <william.wagar@brightspeed.com>
2024-10-17 16:51:03 -0500

    Squashed 'wnwagar3-ContainsDuplicates.git/' content from commit 5378d92
    git-subtree-dir: wnwagar3-ContainsDuplicates.git
    git-subtree-split: 5378d92967fdc5bd507869ef015888c67d76cbea

---
+++ src/main/java/ContainsDuplicates.java
@@ -0,0 +1,22 @@
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContainsDuplicates {
+    /**
+     * Determine if an array of items contains any duplicate values. You should use a Set for this: remember that
+     * Sets can not have duplicate values, but you may check if a value is already contained using the .contains method.
+     * @param nums an array of ints.
+     * @return true if nums contains any duplicate values, false if it does not.
+     */
+    public boolean containsDuplicate(int[] nums){
+        
+        Set<Integer> num_set = new HashSet<>();
+        for (int num: nums){
+            if (num_set.contains(num)){
+                return true;
+            }
+            num_set.add(num);
+        }
+        return false;
+    }
+}
