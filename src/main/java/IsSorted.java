71b8b1fc3428ff565f77a3cc99a0c517c1b8ead1
Wagar <william.wagar@brightspeed.com>
2024-10-17 16:54:42 -0500

    Squashed 'wnwagar3-IsSorted.git/' content from commit 07ed5b4
    git-subtree-dir: wnwagar3-IsSorted.git
    git-subtree-split: 07ed5b4cfeb806e20c5a236a59fc25b20190d3bd

---
+++ src/main/java/IsSorted.java
@@ -0,0 +1,20 @@
+public class IsSorted {
+    /**
+     * Return true if arr is sorted, smallest to largest, false otherwise.
+     * For example, {1,2,3,4} is storted but {1,2,4,3} is unsorted
+     *
+     * @param arr an int array
+     * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
+     */
+    public boolean checkSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++){
+                if (arr[j] < arr[i]){
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
